abf11500ef72971925052502b01438a682a7eeed
younes <errahmani61@gmail.com>
2019-09-26 19:39:11 +0100

    angular carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import * as M from "node_modules/materialize-css/dist/js/materialize.min.js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-deux-project';
+options ={fullWidth: true};
+  ngOnInit(){
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, this.options);
+  }
 }
